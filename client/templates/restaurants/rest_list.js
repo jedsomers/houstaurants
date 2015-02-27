a3e4ba7d76da903cf7cb50ebc3263119b9926590
Jed Somers <somersjed@gmail.com>
2015-02-26 21:39:57 -0500

    finish part 3

---
+++ client/templates/restaurants/rest_list.js
@@ -0,0 +1,16 @@
+var restsData = [
+	{
+		name: 'Coltivare',
+		url: 'http://www.coltivare.com',
+		taRank: 45
+	},
+	{
+		name: 'Oxheart',
+		url: 'http://www.oxheart.com',
+		taRank: 45
+	}
+];
+
+Template.restsList.helpers({
+	rests: restsData
+});
